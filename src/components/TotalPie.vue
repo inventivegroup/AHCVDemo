2e520e998088f8d98442f15c354d0d8e0e54c08a
Simon Timms <stimms@gmail.com>
2021-05-13 21:40:39 -0600

    initial add

---
+++ src/components/TotalPie.vue
@@ -0,0 +1,16 @@
+<script>
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  name: "doughnut",
+  extends: Doughnut,
+  props: ['chartdata', 'options'],
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
